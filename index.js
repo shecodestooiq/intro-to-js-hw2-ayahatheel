e73fe22f284867e35cd22d93b958c2d5b4b44349
Ayah Atheel <ayah.atheel@gmail.com>
2024-02-04 03:14:37 +0300

    amthmatics

---
+++ index.js
@@ -0,0 +1,27 @@
+// 1-Two variables, a and b, are declared with numerical values.
+var a = 10
+var b = 5
+
+console.log("a=", a);
+console.log("b=", b);
+
+//2-Comparison operators are used to compare the values of a and b:
+
+//First Method
+var isAGreaterThanB = a > b;
+console.log("Is a > b?", isAGreaterThanB);
+
+var isALessThanB = a < b;
+console.log("Is a <  b?", isALessThanB);
+
+var isAGreaterThanOrEqualToB = a >= b;
+console.log("Is a >= to b?", isAGreaterThanOrEqualToB);
+
+// Second Short Method
+console.log("Is a <= to b?", a <= b);
+
+console.log("Is a  ==  b?", a == b);
+
+console.log("Is a !=  b?", a != b);
+
+
